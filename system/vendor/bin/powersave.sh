13d526efbbf15001b528dee6d5f9df172ef50e6f
nacizzz <73728553+nacizzz@users.noreply.github.com>
2020-11-19 13:47:42 +0800

    add little_hispeed control

--- system/vendor/bin/powersave.sh
+++ system/vendor/bin/powersave.sh
@@ -1,24 +1,28 @@
 #设置变量
 little_up=/sys/devices/system/cpu/cpufreq/policy0/schedutil/up_rate_limit_us
 little_down=/sys/devices/system/cpu/cpufreq/policy0/schedutil/down_rate_limit_us
+little_hispeed=/sys/devices/system/cpu/cpufreq/policy0/schedutil/hispeed_load
 big_up=/sys/devices/system/cpu/cpufreq/policy6/schedutil/up_rate_limit_us
 big_down=/sys/devices/system/cpu/cpufreq/policy6/schedutil/down_rate_limit_us
 big_hispeed=/sys/devices/system/cpu/cpufreq/policy6/schedutil/hispeed_load
 #设置权限
 chmod 644 $little_up
 chmod 644 $little_down
+chmod 644 $little_hispeed
 chmod 644 $big_up
 chmod 644 $big_down
 chmod 644 $big_hispeed
 #应用参数
 echo 3000 > $little_up
 echo 5000 > $little_down
+echo 95 > $little_hispeed
 echo 3000 > $big_up
 echo 3000 > $big_down
 echo 90 > $big_hispeed
 #设置权限
 chmod 444 $little_up
 chmod 444 $little_down
+chmod 444 $little_hispeed
 chmod 444 $big_up
 chmod 444 $big_down
 chmod 444 $big_hispeed
