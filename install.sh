908298364eec31d95f849ffe920584b7d1d3c485
nacizzz <73728553+nacizzz@users.noreply.github.com>
2020-11-27 10:20:36 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,8 +47,11 @@ REPLACE="
 print_modname() {
   ui_print "*******************************"
   ui_print "        作者：酷安@leleleha      "
-  ui_print "        当前版本:v1.4           "
+  ui_print "        当前版本:v2.0           "
   ui_print "更新日志及其它使用事项可以在酷安动态找到"
+  ui_print "     在重启之前一定一定要去看动态！！！"
+  ui_print "     在重启之前一定一定要去看动态！！！"
+  ui_print "     在重启之前一定一定要去看动态！！！"
   ui_print "*******************************"
 }
 
