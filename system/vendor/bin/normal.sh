07c0a5ada14846812247a527a9b328769b2d218a
nacizzz <73728553+nacizzz@users.noreply.github.com>
2020-11-29 05:17:10 +0800

    Update normal.sh

--- system/vendor/bin/normal.sh
+++ system/vendor/bin/normal.sh
@@ -1,3 +1,20 @@
+function set_default_config() {
+echo "等待系统启动"
+echo " "
+while [ 1 = 1 ]
+do
+sleep 3
+boot_completed=$(getprop sys.boot_completed)
+if [[ "$boot_completed" == "1" ]]; then
+break
+fi
+done
+echo "系统启动完成"
+echo " "
+echo "等待60秒，过早应用配置会权限不足"
+echo " "
+sleep 60
+
 panel=/storage/emulated/0/Android/panel_grus-opt.txt
 powersave=/vendor/bin/powersave.sh
 balance=/vendor/bin/balance.sh
@@ -5,18 +22,29 @@ performance=/vendor/bin/performance.sh
 normal_panel_powersave=`$BusyBox grep 'normal_power_mode=powersave' "$panel"`
 normal_panel_balance=`$BusyBox grep 'normal_power_mode=balance' "$panel"`
 normal_panel_performance=`$BusyBox grep 'normal_power_mode=performance' "$panel"`
-
-if [[ $normal_panel_powersave == normal_power_mode=powersave ]] && [[ $normal_panel_balance != normal_power_mode=balance ]] && [[ $normal_panel_performance != normal_power_mode=performance ]];
+echo "开始应用配置"
+echo " "
+if [[ $normal_panel_powersave == normal_power_mode=powersave ]];
 then
 sh $powersave
+echo "应用省电配置成功"
+echo " "
 fi
-
-if [[ $normal_panel_balance == normal_power_mode=balance ]] && [[ $normal_panel_powersave != normal_power_mode=powersave ]] && [[ $normal_panel_performance != normal_power_mode=performance ]];
+if [[ $normal_panel_balance == normal_power_mode=balance ]];
 then
 sh $balance
+echo "应用平衡配置成功"
+echo " "
 fi
 
-if [[ $normal_panel_performance == normal_power_mode=performance ]] && [[ $normal_panel_powersave != normal_power_mode=powersave ]] && [[ $normal_panel_balance != normal_power_mode=balance ]];
+if [[ $normal_panel_performance == normal_power_mode=performance ]];
 then
 sh $performance
+echo "应用性能配置成功"
+echo " "
 fi
+echo "两条报错为正常现象，请无视"
+echo "全部完成"
+}
+
+set_default_config
