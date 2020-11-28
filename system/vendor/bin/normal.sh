0155962ee822ebaa1cb8d66772e3c0ab0749cf2b
nacizzz <73728553+nacizzz@users.noreply.github.com>
2020-11-28 09:00:14 +0800

    Update normal.sh

--- system/vendor/bin/normal.sh
+++ system/vendor/bin/normal.sh
@@ -1,25 +1,22 @@
 panel=/storage/emulated/0/Android/panel_grus-opt.txt
 powersave=/vendor/bin/powersave.sh
 balance=/vendor/bin/balance.sh
-performance=/vendor/bin/balance.sh
+performance=/vendor/bin/performance.sh
+normal_panel_powersave=`$BusyBox grep 'normal_power_mode=powersave' "$panel"`
+normal_panel_balance=`$BusyBox grep 'normal_power_mode=balance' "$panel"`
+normal_panel_performance=`$BusyBox grep 'normal_power_mode=performance' "$panel"`
 
-normal_panel_powersave=`$BusyBox grep '.*normal_power_mode=powersave*' "$panel"`
-if 
-[ $normal_panel_powersave == normal_power_mode=powersave ];
-then 
+if [[ $normal_panel_powersave == normal_power_mode=powersave ]] && [[ $normal_panel_balance != normal_power_mode=balance ]] && [[ $normal_panel_performance != normal_power_mode=performance ]];
+then
 sh $powersave
 fi
 
-if
-normal_panel_balance=`$BusyBox grep '.*normal_power_mode=balance*' "$panel"`
-[ $normal_panel_balance == normal_power_mode=balance ];
-then 
+if [[ $normal_panel_balance == normal_power_mode=balance ]] && [[ $normal_panel_powersave != normal_power_mode=powersave ]] && [[ $normal_panel_performance != normal_power_mode=performance ]];
+then
 sh $balance
 fi
 
-if
-normal_panel_performance=`$BusyBox grep '.*normal_power_mode=performance*' "$panel"`
-[ $normal_panel_performance == normal_power_mode=performance ];
-then 
+if [[ $normal_panel_performance == normal_power_mode=performance ]] && [[ $normal_panel_powersave != normal_power_mode=powersave ]] && [[ $normal_panel_balance != normal_power_mode=balance ]];
+then
 sh $performance
 fi
