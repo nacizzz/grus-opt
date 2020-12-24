666b8594298b1d91a27548a0e2000c8692339966
nacizzz <73728553+nacizzz@users.noreply.github.com>
2020-12-24 18:48:11 +0800

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -1,6 +1,9 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
 
+if [[ -f $log_file ]]; then
+touch $log_file
+fi
 log_file=/cache/grus-opt.log
 echo -n "" > $log_file
 sh /vendor/bin/normal.sh >> $log_file 2>&1
