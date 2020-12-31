d05b7933a27c2ad4511a5e8a536020d8a6aa4007
nacizzz <73728553+nacizzz@users.noreply.github.com>
2020-12-31 17:20:38 +0800

    Create config.sh

---
+++ file/config.sh
@@ -0,0 +1,10 @@
+little_up=/sys/devices/system/cpu/cpufreq/policy0/schedutil/up_rate_limit_us
+little_down=/sys/devices/system/cpu/cpufreq/policy0/schedutil/down_rate_limit_us
+little_hispeed=/sys/devices/system/cpu/cpufreq/policy0/schedutil/hispeed_load
+big_up=/sys/devices/system/cpu/cpufreq/policy6/schedutil/up_rate_limit_us
+big_down=/sys/devices/system/cpu/cpufreq/policy6/schedutil/down_rate_limit_us
+big_hispeed=/sys/devices/system/cpu/cpufreq/policy6/schedutil/hispeed_load
+cpu4_online=/sys/devices/system/cpu/cpu4/online
+cpu5_online=/sys/devices/system/cpu/cpu5/online
+
+uperf_flag=/data/adb/modules/uperf/module.prop
