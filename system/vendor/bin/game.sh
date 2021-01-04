7f6747b0f35e0a068076f6d2891e91f77ccada5d
nacizzz <73728553+nacizzz@users.noreply.github.com>
2021-01-04 19:29:47 +0800

    Update game.sh

--- system/vendor/bin/game.sh
+++ system/vendor/bin/game.sh
@@ -29,9 +29,7 @@ chmod 444 $cpu5_online
 #应用uperf模式
 if [[ -f $uperf_flag ]]; then
 echo balance > /data/uperf_powermode
-echo '切换到game模式
-'
+log "切换到game模式"
 else
-echo '切换uperf模式失败，请安装uperf
-'
+log "切换uperf模式失败，请安装uperf"
 fi
