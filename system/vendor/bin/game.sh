bc05aa5a5042f1b0e50cc0b5f1a506df90e5ea94
nacizzz <73728553+nacizzz@users.noreply.github.com>
2021-01-07 06:11:44 +0800

    Update game.sh

--- system/vendor/bin/game.sh
+++ system/vendor/bin/game.sh
@@ -26,6 +26,8 @@ chmod 444 $big_down
 chmod 444 $big_hispeed
 chmod 444 $cpu4_online
 chmod 444 $cpu5_online
+#设置schedboost
+echo 1 > $schedboost
 #应用uperf模式
 if [[ -f $uperf_flag ]]; then
 echo balance > /data/uperf_powermode
