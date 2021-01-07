623c51749f83b38be9b8f41c966b32e0b4485d00
nacizzz <73728553+nacizzz@users.noreply.github.com>
2021-01-08 05:33:49 +0800

    Update config.sh

--- file/config.sh
+++ file/config.sh
@@ -4,10 +4,9 @@ little_hispeed=/sys/devices/system/cpu/cpufreq/policy0/schedutil/hispeed_load
 big_up=/sys/devices/system/cpu/cpufreq/policy6/schedutil/up_rate_limit_us
 big_down=/sys/devices/system/cpu/cpufreq/policy6/schedutil/down_rate_limit_us
 big_hispeed=/sys/devices/system/cpu/cpufreq/policy6/schedutil/hispeed_load
-cpu4_online=/sys/devices/system/cpu/cpu4/online
-cpu5_online=/sys/devices/system/cpu/cpu5/online
 uperf_flag=/data/adb/modules/uperf/module.prop
-schedboost=/dev/stune/foreground/schedtune.boost
+gpu_minpowerlever=/sys/class/kgsl/kgsl-3d0/min_pwrlevel
+gpu_minfreq=/sys/class/kgsl/kgsl-3d0/min_clock_mhz
 
 function log()
 {
