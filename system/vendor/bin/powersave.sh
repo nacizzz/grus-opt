62d56ef1ed91a24b35c23a3b46ab8f2676294a9d
nacizzz <73728553+nacizzz@users.noreply.github.com>
2021-01-08 05:38:19 +0800

    Update powersave.sh

--- system/vendor/bin/powersave.sh
+++ system/vendor/bin/powersave.sh
@@ -6,8 +6,6 @@ chmod 644 $little_hispeed
 chmod 644 $big_up
 chmod 644 $big_down
 chmod 644 $big_hispeed
-chmod 644 $cpu4_online
-chmod 644 $cpu5_online
 #应用参数
 echo 2000 > $little_up
 echo 1000 > $little_down
@@ -15,8 +13,6 @@ echo 95 > $little_hispeed
 echo 2000 > $big_up
 echo 1000 > $big_down
 echo 90 > $big_hispeed
-echo 0 > $cpu4_online
-echo 0 > $cpu5_online
 #设置权限
 chmod 444 $little_up
 chmod 444 $little_down
@@ -24,10 +20,9 @@ chmod 444 $little_hispeed
 chmod 444 $big_up
 chmod 444 $big_down
 chmod 444 $big_hispeed
-chmod 444 $cpu4_online
-chmod 444 $cpu5_online
-#设置schedboost
-echo 0 > $schedboost
+#设置gpu
+echo "5" > $gpu_minpowerlever
+echo "180" > $gpu_minfreq
 #提示当前模式
 echo powersave > /cache/grus_opt_mode
 log "切换到powersave模式"
