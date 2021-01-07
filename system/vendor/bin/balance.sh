bd845e81c08b0edcdcb43ceb724037ca5bce1f26
nacizzz <73728553+nacizzz@users.noreply.github.com>
2021-01-08 05:34:50 +0800

    Update balance.sh

--- system/vendor/bin/balance.sh
+++ system/vendor/bin/balance.sh
@@ -7,8 +7,6 @@ chmod 644 $little_hispeed
 chmod 644 $big_up
 chmod 644 $big_down
 chmod 644 $big_hispeed
-chmod 644 $cpu4_online
-chmod 644 $cpu5_online
 #应用参数
 echo 0 > $little_up
 echo 0 > $little_down
@@ -16,8 +14,6 @@ echo 90 > $little_hispeed
 echo 0 > $big_up
 echo 0 > $big_down
 echo 85 > $big_hispeed
-echo 1 > $cpu4_online
-echo 1 > $cpu5_online
 #设置权限
 chmod 444 $little_up
 chmod 444 $little_down
@@ -25,10 +21,9 @@ chmod 444 $little_hispeed
 chmod 444 $big_up
 chmod 444 $big_down
 chmod 444 $big_hispeed
-chmod 444 $cpu4_online
-chmod 444 $cpu5_online
-#设置schedboost
-echo 0 > $schedboost
+#设置gpu
+echo "5" > $gpu_minpowerlever
+echo "180" > $gpu_minfreq
 #提示当前模式
 echo balance > /cache/grus_opt_mode
 log "切换到balance模式"
