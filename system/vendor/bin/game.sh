0d8036ced46d3cacfe0181f243e3fe2ae699bc51
nacizzz <73728553+nacizzz@users.noreply.github.com>
2021-01-08 05:36:27 +0800

    Update game.sh

--- system/vendor/bin/game.sh
+++ system/vendor/bin/game.sh
@@ -6,8 +6,6 @@ chmod 644 $little_hispeed
 chmod 644 $big_up
 chmod 644 $big_down
 chmod 644 $big_hispeed
-chmod 644 $cpu4_online
-chmod 644 $cpu5_online
 #应用参数
 echo 500 > $little_up
 echo 15000 > $little_down
@@ -15,8 +13,6 @@ echo 90 > $little_hispeed
 echo 500 > $big_up
 echo 15000 > $big_down
 echo 85 > $big_hispeed
-echo 1 > $cpu4_online
-echo 1 > $cpu5_online
 #设置权限
 chmod 444 $little_up
 chmod 444 $little_down
@@ -24,16 +20,15 @@ chmod 444 $little_hispeed
 chmod 444 $big_up
 chmod 444 $big_down
 chmod 444 $big_hispeed
-chmod 444 $cpu4_online
-chmod 444 $cpu5_online
-#设置schedboost
-echo 1 > $schedboost
+#设置gpu
+echo "5" > $gpu_minpowerlever
+echo "180" > $gpu_minfreq
 #提示当前模式
 echo game > /cache/grus_opt_mode
 log "切换到game模式"
 #应用uperf模式
 if [[ -f $uperf_flag ]]; then
-echo balance > /data/uperf_powermode
+echo game > /data/uperf_powermode
 else
 log "切换uperf模式失败，请安装uperf"
 fi
