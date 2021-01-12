a7d847c16735350039634895905ce5d52c7f7776
nacizzz <73728553+nacizzz@users.noreply.github.com>
2021-01-13 05:50:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,52 +1,18 @@
-##########################################################################################
-#
-# Magisk Module Installer Script
-#
-##########################################################################################
-##########################################################################################
-#
-# Instructions:
-#
-# 1. Place your files into system folder (delete the placeholder file)
-# 2. Fill in your module's info into module.prop
-# 3. Configure and implement callbacks in this file
-# 4. If you need boot scripts, add them into common/post-fs-data.sh or common/service.sh
-# 5. Add your additional or modified system properties into common/system.prop
-#
-##########################################################################################
-
-##########################################################################################
-# Config Flags
-##########################################################################################
-
-# Set to true if you do *NOT* want Magisk to mount
-# any files for you. Most modules would NOT want
-# to set this flag to true
 SKIPMOUNT=false
 
-# Set to true if you need to load system.prop
 PROPFILE=true
 
-# Set to true if you need post-fs-data script
 POSTFSDATA=true
 
-# Set to true if you need late_start service script
 LATESTARTSERVICE=true
 
-##########################################################################################
-# Replace list
-##########################################################################################
-
-# List all directories you want to directly replace in the system
-# Check the documentations for more info why you would need this
-
-# Construct your own list here
 REPLACE="
 "
 
 print_modname() {
-  ui_print "作者：酷安@leleleha"
-  ui_print "当前版本:v2.0.7"
+  ui_print "- 作者：酷安@leleleha"
+  ui_print "- 当前版本:v2.0.8"
+  ui_print "- 默认性能配置为powersave(省电)"
 }
 
   pm disable com.xiaomi.simactivate.service
@@ -57,17 +23,16 @@ print_modname() {
   pm disable com.qualcomm.wfd.service
   pm disable com.qualcomm.qti.poweroffalarm
   pm disable com.miui.phrase
-  ui_print "已冻结部分应用"
+  ui_print "- 已冻结部分应用"
   
   if [[ -f /data/adb/modules/uperf/module.prop ]];
   then
-  ui_print "检测到已安装uperf"
+  ui_print "- 检测到已安装uperf"
   else
-  ui_print "检测到未安装uperf，建议安装"
-  ui_print "github.com/yc9559/uperf"
+  ui_print "! 检测到未安装uperf，建议安装"
+  ui_print "! github.com/yc9559/uperf"
   fi
 
-#创建配置文件
   touch /storage/emulated/0/Android/panel_grus-opt.txt
   echo "
 #修改开机默认性能配置
@@ -78,27 +43,14 @@ print_modname() {
 normal_power_mode=powersave
 " > /storage/emulated/0/Android/panel_grus-opt.txt
 
-#应用uperf开机默认配置
-#在开机时以最高性能运行，减少卡顿
   echo "default_mode=performance" > /storage/emulated/0/Android/panel_uperf.txt
 
-# Copy/extract your module files into $MODPATH in on_install.
-
 set_permissions() {
-  # The following is the default rule, DO NOT remove
   set_perm_recursive $MODPATH/system 0 0 0755 0644
-
-  # Here are some examples:
-  # set_perm_recursive  $MODPATH/system/lib       0     0       0755      0644
-  # set_perm  $MODPATH/system/bin/app_process32   0     2000    0755      u:object_r:zygote_exec:s0
-  # set_perm  $MODPATH/system/bin/dex2oat         0     2000    0755      u:object_r:dex2oat_exec:s0
-  # set_perm  $MODPATH/system/lib/libart.so       0     0       0644
 }
 
 on_install() {
-  # The following is the default implementation: extract $ZIPFILE/system to $MODPATH
-  # Extend/change the logic to whatever you want
-  ui_print "- Extracting module files"
+  ui_print "- 解压文件中"
   unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
   unzip -o "$ZIPFILE" 'file/*' -d $MODPATH >&2
 }
